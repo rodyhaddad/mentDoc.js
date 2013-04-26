0be6bf1365b396a5fd9527f46bffc6af9051a053
Rody Haddad <rody@rodyhaddad.com>
2013-04-26 00:49:26 -0400

    Added helper methods, switched code to using them and renamed some vars

--- src/mentDoc.js
+++ src/mentDoc.js
@@ -1,7 +1,77 @@
 var mentDoc = (function(){
 	
-	
 	return {
+	    _getCommandsEl: function (containerEl) {
+    	    return Array.prototype.slice.call(
+    	       containerEl.getElementsByTagName("u");
+    	    )
+	    },
+    	compile: function (html) {
+        	var commandsEl, containerEl;
+        	
+        	
+        	containerEl = document.createElement("div");    
+        	containerEl.innerHTML = html;
+        	
+        	commandsEl = this._getCommandsEl(holder);
+        	
+        	forEach(commandsEl, function(el) {
+            	var attrs = el.attributes;
+            	
+            	forEach(attrs, function(attr) {
+                	if (attr.specified) {
+                    	var name = attr.name,
+                    	    value = el.getAttribute(name, 3);
+                	}
+            	});
+        	});
+        	
+    	},
     	
+    	
+    	
+    	
+    	forEach: forEach,
+    	isArray: isArray,
+    	isObject: isObject,
+    	isFn: isFn
 	};
+	
+	function forEach(obj, iterator, context) {
+        var key;
+        if(obj) {
+            if(isFn(obj)) {
+                for(key in obj) {
+                    if(key != "prototype" && key != "length" && key != "name" && obj.hasOwnProperty(key)) {
+                        iterator.call(context, obj[key], key);
+                    }
+                }
+            } else if (obj.forEach && obj.forEach !== forEach) {
+                obj.forEach(iterator, context);
+            } else if (isArray(obj) || obj.hasOwnProperty("length")) {
+                for(key = 0; key < obj.length; key++)
+                    iterator.call(context, obj[key], key);
+            } else {
+                for(key in obj) {
+                    if(obj.hasOwnProperty(key)) {
+                        iterator.call(context, obj[key], key);
+                    }
+                }
+            }
+        }
+        return obj;
+    };
+    
+    function isArray(obj) {
+        return Object.prototype.toString.call(obj) === "[object Array]";
+    };
+
+    function isObject(obj) {
+        return typeof obj === "object" && obj !== null;
+    };
+    
+    function isFn(fn) {
+        return typeof fn === "function";
+    };
+	
 }());
