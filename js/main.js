00a6ab106ea85d87f722df67ac2ab071e2e5ae40
Rody Haddad <rody@rodyhaddad.com>
2013-05-16 10:20:35 -0400

    Started Overview and Usage API sections

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+$(".markdown").each(function() {
+    var content = $(this).html(),
+        html = mentDoc.markdown.convertHtml(content);
+    
+    
+    $(this).empty().html(html);
+})
+
+$(".mentDoc").each(function() {
+    mentDoc.compile(this.innerHTML).execute();
+})
