08146d4309372b66a361630e90a030778798c258
rodyhaddad <rody@rodyhaddad.com>
2013-05-23 17:17:53 -0400

    Almost finished documenting default directives

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ $(".markdown").each(function() {
     var content = $(this).html(),
         html = mentDoc.markdown.convertHtml(content);
     
+    // This is a total hack, I know
+    // markdown="" is ugly to show, but that's what `.innerHTML` returns
+    // so I'm fixing it
+    html = html.replace(/ markdown=""/, " markdown");
+    
     $(this).empty().html(html);
 });
 
